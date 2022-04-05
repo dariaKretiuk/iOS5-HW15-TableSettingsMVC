c9735d549b11e3d3d7b51f6e3a207219f96f656a
dariaKretiuk <kretyukdasha@gmail.com>
2022-04-05 21:35:26 +0700

    Add View in project

---
+++ iOS5-HW15-TableSettingsMVC/Sources/Scenes/View/SwitchTableViewCell.swift
@@ -0,0 +1,110 @@
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+    
+    static let identifier = "SwitchTableViewCell"
+    
+    // MARK: - Configuration
+    
+    public func configure(with model: SettingsSwitchOption) {
+        nameSetting.text = model.title
+        settingImageView.image = UIImage(systemName: model.icon)
+        settingImageView.backgroundColor = UIColor(named: model.iconBackgroundColor)
+        additionalSwitch.isOn = model.isOn
+    }
+    
+    // MARK: - View
+    
+    private let settingImageViewContainer: UIView = {
+       let view = UIView()
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    private let settingImageView: UIImageView = {
+        let img = UIImageView()
+        img.contentMode = .left
+        img.contentMode = .center
+        img.layer.cornerRadius = 10
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.tintColor = .white
+        return img
+    }()
+    
+    private let nameSetting: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let additionalSwitch: UISwitch = {
+        let mySwitch = UISwitch()
+        mySwitch.contentMode = .center
+        return mySwitch
+    }()
+    
+    private var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 15
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    // MARK: - Initial
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupHierarchy()
+        setupLayouts()
+        contentView.clipsToBounds = true
+        accessoryType = .disclosureIndicator
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        settingImageView.image = nil
+        nameSetting.text = nil
+        settingImageViewContainer.backgroundColor = nil
+        additionalSwitch.isOn = false
+    }
+    
+    // MARK: - Settings
+    
+    private func setupHierarchy() {
+        contentView.addSubview(stackView)
+        stackView.addArrangedSubview(settingImageViewContainer)
+        stackView.addArrangedSubview(nameSetting)
+        stackView.addArrangedSubview(additionalSwitch)
+        settingImageViewContainer.addSubview(settingImageView)
+    }
+    
+    private func setupLayouts() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -45),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            stackView.leftAnchor.constraint(equalTo: leftAnchor),
+            settingImageViewContainer.centerYAnchor.constraint(equalTo: centerYAnchor),
+            settingImageViewContainer.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
+            settingImageViewContainer.widthAnchor.constraint(equalToConstant: 40),
+            settingImageViewContainer.heightAnchor.constraint(equalToConstant: 40),
+            settingImageView.centerYAnchor.constraint(equalTo: settingImageViewContainer.centerYAnchor),
+            settingImageView.centerXAnchor.constraint(equalTo: settingImageViewContainer.centerXAnchor),
+            settingImageView.widthAnchor.constraint(equalToConstant: 35),
+            settingImageView.heightAnchor.constraint(equalToConstant: 35),
+            nameSetting.centerYAnchor.constraint(equalTo: stackView.centerYAnchor),
+            nameSetting.leftAnchor.constraint(equalTo: settingImageViewContainer.rightAnchor, constant: 5),
+            nameSetting.trailingAnchor.constraint(equalTo: additionalSwitch.leadingAnchor),
+            additionalSwitch.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 6),
+            additionalSwitch.rightAnchor.constraint(equalTo: stackView.rightAnchor)
+        ])
+    }
+}
+
