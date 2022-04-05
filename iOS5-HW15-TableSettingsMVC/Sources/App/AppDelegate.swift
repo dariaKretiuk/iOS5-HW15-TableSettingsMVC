42de009d638e69a9782799641b5cf8493f41b10c
dariaKretiuk <kretyukdasha@gmail.com>
2022-04-05 22:09:49 +0700

    Add Controller in project

--- iOS5-HW15-TableSettingsMVC/Sources/App/AppDelegate.swift
+++ iOS5-HW15-TableSettingsMVC/Sources/App/AppDelegate.swift
@@ -1,21 +1,34 @@
-//
-//  AppDelegate.swift
-//  iOS5-HW15-TableSettingsMVC
-//
-//  Created by Дарья Кретюк on 05.04.2022.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    var window: UIWindow?
+    
+    private let appearance: UINavigationBarAppearance =  {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(red: 247.0/255.0,
+                                             green: 247.0/255.0,
+                                             blue: 247.0/255.0,
+                                             alpha: 1
+        )
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.black]
+        return appearance
+    }()
+    
+    private func settingsNavigationBar() {
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame:UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: TableSettingsController())
+        window?.makeKeyAndVisible()
+        settingsNavigationBar()
+        return true
+    }
 }
 
